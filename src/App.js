af225be3f9c4885e7d70fc155faa9dcbbb76ad3e
ComradeBarry <axel.renwart@gmail.com>
2025-01-08 13:41:23 +0100

    Update App.js to use FilterableMap component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import FilterableMap from './components/Map/FilterableMap';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <FilterableMap />
+    </div>
+  );
+}
+
+export default App;
