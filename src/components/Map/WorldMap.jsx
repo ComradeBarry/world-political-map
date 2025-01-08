15293602779cb1ab55b4857cb8edf5b226e87b6e
ComradeBarry <axel.renwart@gmail.com>
2025-01-08 10:10:04 +0100

    Add initial project files

---
+++ src/components/Map/WorldMap.jsx
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from 'react';
+import { MapPin } from 'lucide-react';
+
+const WorldMap = () => {
+  // Component code will be added here
+  return <div>Map Component</div>;
+};
+
+export default WorldMap;
---
+++ src/services/mapService.js
@@ -0,0 +1,4 @@
+// Initial map service
+export const initializeMap = () => {
+  // Service code will be added here
+};
